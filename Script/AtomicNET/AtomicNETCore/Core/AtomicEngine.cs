0bd29f55edd5b79e447bec6f26d75e356c7b5017
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-06-20 16:02:58 -0700

    Support bindings which are exclusively JS/C#

--- Script/AtomicNET/AtomicNETCore/Core/AtomicEngine.cs
+++ Script/AtomicNET/AtomicNETCore/Core/AtomicEngine.cs
@@ -5,56 +5,56 @@ using System.Runtime.InteropServices;
 namespace AtomicEngine
 {
 
-static class NativeCore
-{
-    // wraps an existing native instance, with downcast support
-    public static T WrapNative<T> (IntPtr native) where T:RefCounted
+    static class NativeCore
     {
-        return null;
+        // wraps an existing native instance, with downcast support
+        public static T WrapNative<T>(IntPtr native) where T : RefCounted
+        {
+            return null;
+        }
+
+        public static void RegisterNativeType(Type type)
+        {
+
+        }
+
+        public static bool GetNativeType(Type type)
+        {
+            return true;
+        }
+
+
+        public static IntPtr RegisterNative(IntPtr native, RefCounted r)
+        {
+            return IntPtr.Zero;
+        }
+
+        public static Dictionary<IntPtr, Func<IntPtr, RefCounted>> nativeClassIDToManagedConstructor = new Dictionary<IntPtr, Func<IntPtr, RefCounted>>();
     }
 
-    public static void RegisterNativeType (Type type)
+    public partial class RefCounted
     {
-      
+
+        public RefCounted()
+        {
+        }
+
+        protected RefCounted(IntPtr native)
+        {
+            nativeInstance = native;
+        }
+
+        public IntPtr nativeInstance = IntPtr.Zero;
+
+        [DllImport(Constants.LIBNAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern IntPtr csb_Atomic_RefCounted_GetClassID(IntPtr self);
+
     }
 
-    public static bool GetNativeType (Type type)
+    public static partial class Constants
     {
-      return true;
+        public const string LIBNAME = "/Users/josh/Dev/atomic/build-AtomicGameEngine-Desktop-Debug/Source/AtomicNET/AtomicNETNative/libAtomicNETNative.dylib";
     }
-
-
-    public static IntPtr RegisterNative (IntPtr native, RefCounted r)
-    {
-        return IntPtr.Zero;
-    }
-
-    public static Dictionary<IntPtr, Func<IntPtr, RefCounted>> nativeClassIDToManagedConstructor = new Dictionary<IntPtr, Func<IntPtr, RefCounted>>();
-}
-
-public partial class RefCounted
-{
-
-    public RefCounted ()
-    {
-    }
-
-    protected RefCounted (IntPtr native)
-    {
-        nativeInstance = native;
-    }
-
-    public IntPtr nativeInstance = IntPtr.Zero;
-
-    [DllImport (Constants.LIBNAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-    public static extern IntPtr csb_Atomic_RefCounted_GetClassID (IntPtr self);
-
-}
-
-public static partial class Constants
-{
-    public const string LIBNAME = "/Users/josh/Dev/atomic/build-AtomicGameEngine-Desktop-Debug/Source/AtomicNET/AtomicNETNative/libAtomicNETNative.dylib";
-}
 
 
 }
--- Script/AtomicNET/AtomicNETCore/Program.cs
+++ Script/AtomicNET/AtomicNETCore/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
             // Thread.Sleep(30 * 1000);
             var debugBuild = AtomicEngine.Engine.GetDebugBuild();
             Console.WriteLine("Debug Build: {0}", debugBuild);
